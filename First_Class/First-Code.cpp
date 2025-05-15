c5f3ebc7809f2ff6056e903deadbff3bb59b22ba
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-15 07:29:06 -0600

    Add compiler information comments to First-Code.cpp

--- First_Class/First-Code.cpp
+++ First_Class/First-Code.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-
+//  Compiler: a program that translates source code into binary
+// there are several compilers available for C++ such as GCC, Clang, MSVC, etc.
+// -msvc is the Microsoft Visual C++ compiler
+// -g++ is the GNU C++ compiler
+// -clang++ is the Clang C++ compiler
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
