5fbfc297922ba9fdaf55ba752d7a7c3bb2e46b1a
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-15 07:48:47 -0600

    Add comments to clarify the purpose of Animal.cpp and Animal.h files

---
+++ First_Class/Animal.cpp
@@ -0,0 +1,3 @@
+// source file (.cpp) - Contains the specific impleementation, here is where we define the HOW
+
+// IMPORTANT - THEY must be related EXPLICITILY
--- First_Class/Animal.h
+++ First_Class/Animal.h
@@ -1,5 +1,8 @@
 // header files (.h) : Contains the definition of the class with NO implementation (unless its inline) - Definir que es lo que hace la clase
 
+#pragma once // means only have a single copy of this file when running
+// ANYTHING THAT STARTS with a # is a preprocessor directive
+// you can think of it as an instruction for the compiler
 class Animal
 {
 };
