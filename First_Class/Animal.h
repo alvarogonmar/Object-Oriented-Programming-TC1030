705324f78e4aca341d3f962cf430e74cd0c80ed7
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-15 07:58:27 -0600

    Add detailed comments to Animal.h for clarity on class definition and naming conventions

--- First_Class/Animal.h
+++ First_Class/Animal.h
@@ -1,8 +1,14 @@
 // header files (.h) : Contains the definition of the class with NO implementation (unless its inline) - Definir que es lo que hace la clase
 
-#pragma once // means only have a single copy of this file when running
+#pragma once // means only have a single copy of this file when running PONERLO EN TODOS LOS .h files
 // ANYTHING THAT STARTS with a # is a preprocessor directive
 // you can think of it as an instruction for the compiler
+
+// a couple of notes on naming conventions:
+// classes should be named using PascalCode - NO SPACES IN NAMES
+// VARIABLES and objects use camelCase
+// constants and enums use UPPER_CASE
 class Animal
 {
+    // INSIDE a class we define MEMBERS!
 };
