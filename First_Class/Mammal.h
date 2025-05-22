f6fb9c0a9da42786638d1c8f3ff07334bdb6c682
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-22 07:54:56 -0600

    Add comments to Mammal class header explaining inheritance access modifiers

--- First_Class/Mammal.h
+++ First_Class/Mammal.h
@@ -1,6 +1,11 @@
 // Inheritance: a class inherits members from a superclass, meaning the subclass can use members of the superclass
 #include "Animal.h"
 
+// When doing inheritance you specify a modifier which changes the members acces modifiers
+// remember: we only inherit public and protected members
+// public - pub;ic remains public, protected remains protected
+// protected - public becomes protected
+// private - everything becomes private
 class Mammal : public Animal
 {
 };
