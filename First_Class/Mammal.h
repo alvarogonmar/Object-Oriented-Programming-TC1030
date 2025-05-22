8a8b6b2c9c61bb9d0d942592961fd6cffc4585e8
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-22 08:01:05 -0600

    Update Mammal class to include Viviparous inheritance and adjust comments

--- First_Class/Mammal.h
+++ First_Class/Mammal.h
@@ -1,11 +1,14 @@
 // Inheritance: a class inherits members from a superclass, meaning the subclass can use members of the superclass
 #include "Animal.h"
+#include "Viviparous.h"
 
 // When doing inheritance you specify a modifier which changes the members acces modifiers
 // remember: we only inherit public and protected members
 // public - pub;ic remains public, protected remains protected
 // protected - public becomes protected
 // private - everything becomes private
-class Mammal : public Animal
+
+// c++ has multiple inheritance which means a clas can be a subclass of several superclasses
+class Mammal : public Animal, public Viviparous
 {
 };
