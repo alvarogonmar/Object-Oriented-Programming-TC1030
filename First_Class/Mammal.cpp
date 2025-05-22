1fd476a17a188197597cf5c700bd94459c844234
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-22 08:20:05 -0600

    Add 'name' member initialization in Mammal constructor

--- First_Class/Mammal.cpp
+++ First_Class/Mammal.cpp
@@ -1,6 +1,6 @@
 #include "Mammal.h"
 
-Mammal::Mammal(int age, int weight, std::string name) : Animal(age, weight)
+Mammal::Mammal(int age, int weight, std::string name) : Animal(age, weight), name(name)
 {
     /// a super constructor is ALWAYS invoked, if its not done explicitely it will invoke the default
     std::cout << "THE MAMMAL CONSTRUCTOR" << std::endl;
