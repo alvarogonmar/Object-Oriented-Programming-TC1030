41b1e1f6191f16b64d725357222f66ee04fbd069
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-22 08:10:10 -0600

    Add iostream inclusion in Mammal.h and ensure proper class declaration

--- First_Class/Mammal.h
+++ First_Class/Mammal.h
@@ -1,6 +1,7 @@
 // Inheritance: a class inherits members from a superclass, meaning the subclass can use members of the superclass
 #include "Animal.h"
 #include "Viviparous.h"
+#include "iostream"
 
 // When doing inheritance you specify a modifier which changes the members acces modifiers
 // remember: we only inherit public and protected members
@@ -11,4 +12,6 @@
 // c++ has multiple inheritance which means a clas can be a subclass of several superclasses
 class Mammal : public Animal, public Viviparous
 {
+public:
+    Mammal(int age, int weight, std::string name);
 };
