4b4af538a8c49b0e84603c9bd37fb6f97b162812
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-22 20:05:48 -0600

    Add constructor to MobileDevice class for initialization of attributes

--- Activity_2_Inheritance/MobileDevice.h
+++ Activity_2_Inheritance/MobileDevice.h
@@ -10,4 +10,7 @@ class MobileDevice : public Computer, public Camera
 protected:
     string brand;
     int screenSize;
+
+public:
+    MobileDevice(int ram, double speed, double size, string brand, double screen) : Computer(ram, speed), Camera(size), brand(brand), screenSize(screen) {}
 };
