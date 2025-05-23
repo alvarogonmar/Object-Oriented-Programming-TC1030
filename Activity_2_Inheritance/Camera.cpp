dc1c4bf7419ad635f52204e2f179dbb0a8216b18
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-22 19:49:19 -0600

    Add Camera class usage in main function and include necessary headers

--- Activity_2_Inheritance/Camera.cpp
+++ Activity_2_Inheritance/Camera.cpp
@@ -1,3 +1,10 @@
-#include "Computer.h"
+#include "Camera.h"
 
 using namespace std;
+
+Camera::Camera(double size) : lensSize(size) {}
+
+void Camera::takePicture()
+{
+    cout << "Picture taked with the lensSize of " << lensSize << "mm" << endl;
+}
--- Activity_2_Inheritance/Project.cpp
+++ Activity_2_Inheritance/Project.cpp
@@ -1,9 +1,13 @@
 #include <iostream> // in - input / output
 #include <string>
 #include "Computer.h"
+#include "Camera.h"
 
 int main()
 {
     Computer computer1(16, 20);
     computer1.turnOn();
+
+    Camera camera1(19.20);
+    camera1.takePicture();
 }
