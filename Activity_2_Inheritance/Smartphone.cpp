51a0c24fe755cf4e9095028c208696c88a07d77e
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-24 14:26:06 -0600

    Fix formatting and add missing newline at end of Smartphone.cpp

--- Activity_2_Inheritance/Smartphone.cpp
+++ Activity_2_Inheritance/Smartphone.cpp
@@ -1,3 +1,12 @@
 #include "Smartphone.h"
 #include <iostream>
 using namespace std;
+
+Smartphone::Smartphone(int ram, double speed, double lens, string brand, double screen, string carrier)
+    : MobileDevice(ram, speed, lens, brand, screen), carrier(carrier) {}
+
+void Smartphone::takePicture()
+{
+    cout << "Smartphone with carrier: " << carrier << " - ";
+    MobileDevice::takePicture(); // Invoking super class version
+}
