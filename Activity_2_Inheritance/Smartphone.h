04404b43834b382dac4daf4caffa62c7d2753cf6
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-24 14:24:43 -0600

    Refactor Smartphone class: include MobileDevice header and update class definition

--- Activity_2_Inheritance/Smartphone.h
+++ Activity_2_Inheritance/Smartphone.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <iostream>
-
-class Smartphone
+#include "MobileDevice.h"
+class Smartphone : public MobileDevice
 {
 protected:
+    string carrier;
+
 public:
+    Smartphone(int ram, double speed, double lens, string brand, double screen, string carrier);
+    void takePicture() override;
 };
