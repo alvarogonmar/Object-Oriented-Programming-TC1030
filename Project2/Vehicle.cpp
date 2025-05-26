627b7484eeb0325d55306ec98f96eaba16f46c2c
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-26 08:07:47 -0600

    Implement Vehicle class constructor and destructor with console output

---
+++ Project2/Vehicle.cpp
@@ -0,0 +1,11 @@
+#include "Vehicle.h"
+// Constructor
+Vehicle::Vehicle()
+{
+    cout << "Vehicle constructor" << endl;
+};
+// destructor
+Vehicle::~Vehicle()
+{
+    cout << "Vehicle destructr" << endl;
+};
