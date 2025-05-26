e7938154eec1e6f77360d98cfe452f580df57063
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-26 07:48:23 -0600

    adding cin.ignore to clean buffer

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -12,9 +12,13 @@ int main()
     cout << "Este es mi codigo para el proyecto" << endl;
 
     int userOption = 0;
-
     do
     {
+        cout << "Choose a number" << endl;
+        cin >> userOption;
 
+        // we need to flush the line before we read a whole line
+        // cin.ignore(amountOfCharactersToIgnore, characterToSearchFor);
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // sirve para limpiar la memoria del buffer
     } while (userOption != 9);
 }
