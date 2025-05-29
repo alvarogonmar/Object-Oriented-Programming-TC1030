0fc32f7e978d840ec5a6f4213b1df00c59ab177c
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-29 08:27:17 -0600

    Add OOPUtils class with utility functions for string manipulation

---
+++ Project2/OOPUtils.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+class OOPUtils
+{
+public:
+    static void cleanCin();
+    static vector<string> split(string delimiter);
+};
