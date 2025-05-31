8d6a980c13c855ea7a4173a5ea5cb048f093176b
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-31 17:34:57 -0600

    Fix constructor definition for Student class

--- Activity_3_TheSchool_DB/Student.cpp
+++ Activity_3_TheSchool_DB/Student.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+Student::Student(string name, int age, string enrollmentId) : Person(name, age), enrollmentId(enrollmentId) {}
+
 string Student::toString() const
 {
     return "Name: " + name + " Enrollment ID: " + enrollmentId;
