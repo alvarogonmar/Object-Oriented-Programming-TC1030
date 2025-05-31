57e5bc5b28c00fa5fdb617d7b3919ca7b534484d
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-31 17:39:35 -0600

    Add a newline for better readability in Teacher.cpp

--- Activity_3_TheSchool_DB/Teacher.cpp
+++ Activity_3_TheSchool_DB/Teacher.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 Teacher::Teacher(string name, int age, string teacherId) : Person(name, age), teacherId(teacherId) {}
+
 string Teacher::toString() const
 {
     return "Name: " + name + " Enrollment ID: " + teacherId;
