5441cc96a36a34dcabba32139f4005d87e61cbf8
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-30 23:18:04 -0600

    Fix typo in constructor parameter name for Student class in Student.h

--- Activity_3_TheSchool_DB/Student.h
+++ Activity_3_TheSchool_DB/Student.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <iostream>
+using namespace std;
 #include "Person.h"
 
 class Student : public Person
 {
 private:
     double enrollmentId;
+
+public:
+    Student(string name, int age, double ennrollmentId);
 };
