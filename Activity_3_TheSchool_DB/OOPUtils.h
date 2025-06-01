7091d415cfecdf7543626b8062b3807e5ec6b5af
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-31 18:02:42 -0600

    Add OOPUtils class with static methods for testing, input cleaning, and string splitting

---
+++ Activity_3_TheSchool_DB/OOPUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class OOPUtils
+{
+public:
+    static void staticTest();
+    static void cleanCin();
+    static vector<string> split(string delimiter);
+};
