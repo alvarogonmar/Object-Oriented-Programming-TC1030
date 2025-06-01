c59086ca6f8ba3e0fa6f5e1a830e3420ee4e4ba3
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-31 18:27:04 -0600

    Add addTeacher function to handle teacher addition and update function declarations

--- Activity_3_TheSchool_DB/Functions.cpp
+++ Activity_3_TheSchool_DB/Functions.cpp
@@ -1,4 +1,5 @@
 #include "Student.h"
+#include "Teacher.h"
 #include <iostream>
 
 void addStudent(vector<Person *> peopleContainer)
@@ -21,4 +22,26 @@ void addStudent(vector<Person *> peopleContainer)
 
     peopleContainer.push_back(new Student(name, age, enrollmentId));
     cout << "Student added successfully!\n";
+}
+
+void addTeacher(vector<Person *> peopleContainer)
+{
+    string name, teacherId;
+    int age;
+
+    cout << "Enter Teacher name: ";
+    cin >> name;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, name);
+
+    cout << "Enter Teacher age: ";
+    cin >> age;
+    cin.ignore();
+
+    cout << "Enter Teacher ID: ";
+    cin >> teacherId;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    peopleContainer.push_back(new Teacher(name, age, teacherId));
+    cout << "Teacher added successfully!\n";
 }
--- Activity_3_TheSchool_DB/Functions.h
+++ Activity_3_TheSchool_DB/Functions.h
@@ -4,3 +4,4 @@ using namespace std;
 #include "Person.h"
 
 void addStudent(vector<Person *> &peopleContainer);
+void addTeacher(vector<Person *> &peopleContainer);
