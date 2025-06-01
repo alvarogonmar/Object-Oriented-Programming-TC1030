1ea9bd8c02a6306e4ebb827f45adbce6c77e5fcf
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-31 18:12:13 -0600

    Implement addStudent function to collect and store student information

--- Activity_3_TheSchool_DB/AddStudent.cpp
+++ Activity_3_TheSchool_DB/AddStudent.cpp
@@ -1,4 +1,24 @@
 #include "Student.h"
 #include <iostream>
 
-void addStudent(vector<Person *> peopleContainer) {}
+void addStudent(vector<Person *> peopleContainer)
+{
+    string name, enrollmentId;
+    int age;
+
+    cout << "Enter Student name: ";
+    cin >> name;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, name);
+
+    cout << "Enter Student age: ";
+    cin >> age;
+    cin.ignore();
+
+    cout << "Enter Student Enrollment ID: ";
+    cin >> enrollmentId;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    peopleContainer.push_back(new Student(name, age, enrollmentId));
+    cout << "Student added successfully!\n";
+}
