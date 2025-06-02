23470e156b70dcf7a046ff19d7f0c0a4fa37d415
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-02 07:19:40 -0600

    Add comment to clarify purpose of split function in OOPUtils

--- Project2/OOPUtils.cpp
+++ Project2/OOPUtils.cpp
@@ -7,6 +7,8 @@ void OOPUtils::cleanCin()
 {
     cin.ignore(numeric_limits<streamsize>::max(), '\n'); // sirve para limpiar la memoria del buffer
 };
+
+// receive a string, breal it apart using the delimiter as a breakpoint, return a vector with the parts.
 vector<string> OOPUtils::split(string delimiter)
 {
     vector<string> result;
